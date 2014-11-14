6110505d720098a2f7f45654a8321e51c40122fd
antti.vartiainen@digia.com <antti.vartiainen@digia.com>
2014-11-14 15:02:02 +0200

    re-scripted startup scripts and configurations to comply with MAR $PORT variable

--- java.sh
+++ java.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+DEFAULT_PORT=8080
+if [ -z "$PORT" ]; then
+	PORT=$DEFAULT_PORT
+fi
+# change the default 8080 HTTP port to align with the container $PORT
+sed s/8080/$PORT/ apache-tomcat/conf/server.xml > apache-tomcat/conf/server_MAR.xml
+mv apache-tomcat/conf/server_MAR.xml apache-tomcat/conf/server.xml
+
 export JAVA_OPTS='-Xmx256m'
 cd apache-tomcat/bin
 ./catalina.sh run
